251bc0ee991766e95e6ec03fdfb565706b702fee
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-04-12 16:26:21 +0700

    Implement Mousetrap

--- modules/utils.ts
+++ modules/utils.ts
@@ -1,4 +1,6 @@
+/* eslint-disable no-restricted-syntax */
 import { Context } from '@nuxt/types';
+import mousetrap from 'mousetrap';
 import * as windows from './electron/windows';
 import * as routers from './pages/routers';
 import { Action } from '~/modules/utils.type';
@@ -14,5 +16,28 @@ export class AppUtils {
 
     constructor(context: Context) {
         this.context = context;
+        this.resetMousetrap();
+    }
+
+    public resetMousetrap() {
+        mousetrap.reset();
+        const dictionary: { [key: string]: string } = {};
+        for (const name in actions) {
+            if (Object.prototype.hasOwnProperty.call(actions, name)) {
+                const action = actions[name as keyof typeof actions];
+                const { call, accelerator } = action;
+                if (accelerator !== undefined) {
+                    if (dictionary[accelerator] !== undefined) {
+                        console.error(
+                            `Shortcut '${accelerator}' has been registered by action '${name}'`,
+                        );
+                    }
+                    dictionary[accelerator] = name;
+                    mousetrap.bind(accelerator, () => {
+                        call.apply(this.context);
+                    });
+                }
+            }
+        }
     }
 }
