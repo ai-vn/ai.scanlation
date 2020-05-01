4851ba3250a469f64789beb1ea2068108ea04486
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-05-01 10:27:16 +0700

    Jest: Clean config

--- jest.config.js
+++ jest.config.js
@@ -1,154 +1,54 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
-module.exports = {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // Respect "browser" field in package.json when resolving modules
-    // browser: false,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "C:\\Users\\phamhongphuc\\AppData\\Local\\Temp\\jest",
-
-    // Automatically clear mock calls and instances between every test
+const common = {
     clearMocks: true,
 
-    //* coverage
-    collectCoverage: true,
-    collectCoverageFrom: [
-        '**/*.{ts,tsx,vue}',
-        '!**/*.d.ts',
-        '!**/node_modules/**',
-        '!**/vendor/**',
-    ],
-    coverageDirectory: 'coverage',
-    coveragePathIgnorePatterns: ['nuxt.config.ts', 'gulpfile.babel.ts'],
-    coverageReporters: ['cobertura', 'html'],
-    // coverageThreshold: null,
-    // forceCoverageMatch: [],
-
     errorOnDeprecated: true,
-
-    //* global
-    // globalSetup: null,
-    // globalTeardown: null,
-    // globals: {},
-
     moduleFileExtensions: ['js', 'ts', 'json', 'vue'],
 
     moduleNameMapper: {
         '~/(.*)$': '<rootDir>/$1',
     },
-
-    // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
-    // modulePathIgnorePatterns: [],
-
-    // Activates notifications for test results
-    // notify: false,
-
-    // An enum that specifies notification mode. Requires { notify: true }
-    // notifyMode: "failure-change",
-
-    // A preset that is used as a base for Jest's configuration
-    // preset: null,
-
-    // Run tests from one or more projects
-    // projects: null,
-
-    // Use this configuration option to add custom reporters to Jest
     reporters: ['default', 'jest-junit'],
-
-    // Automatically reset mock state between every test
-    // resetMocks: false,
-
-    // Reset the module registry before running each individual test
-    // resetModules: false,
-
-    // A path to a custom resolver
-    // resolver: null,
-
-    // Automatically restore mock state between every test
-    // restoreMocks: false,
-
-    // The root directory that Jest should scan for tests and modules within
-    // rootDir: null,
-
-    // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
-
-    // Allows you to use a custom runner instead of Jest's default test runner
-    // runner: "jest-runner",
-
-    // The paths to modules that run some code to configure or set up the testing environment before each test
-    // setupFiles: [],
-
-    // A list of paths to modules that run some code to configure or set up the testing framework before each test
     setupFilesAfterEnv: ['jest-extended'],
-
-    // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-    // snapshotSerializers: [],
-
-    // The test environment that will be used for testing
     testEnvironment: 'jsdom',
-
-    // Options that will be passed to the testEnvironment
-    // testEnvironmentOptions: {},
-
-    // Adds a location field to test results
-    // testLocationInResults: false,
-
-    // The glob patterns Jest uses to detect test files
-    // testMatch: [
-    //   "**/__tests__/**/*.[jt]s?(x)",
-    //   "**/?(*.)+(spec|test).[tj]s?(x)"
-    // ],
-
-    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-    // testPathIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
-
-    // The regexp pattern or array of patterns that Jest uses to detect test files
-    // testRegex: [],
-
-    // This option allows the use of a custom results processor
-    // testResultsProcessor: null,
-
-    // This option allows use of a custom test runner
-    // testRunner: "jasmine2",
-
-    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-    // testURL: "http://localhost",
-
-    // Setting this value to "fake" allows the use of fake timers for functions such as "setTimeout"
-    // timers: "real",
-
-    // A map from regular expressions to paths to transformers
     transform: {
-        '^.+\\.(vue)$': 'vue-jest',
-        '^.+\\.(js|ts|tsx)$': 'ts-jest',
+        '^.+\\.vue$': 'vue-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
+    preset: 'ts-jest/presets/js-with-babel',
+    globals: { 'ts-jest': { babelConfig: true } },
+};
 
-    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-    // transformIgnorePatterns: [
-    //   "\\\\node_modules\\\\"
-    // ],
-
-    // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
-    // unmockedModulePathPatterns: undefined,
-
-    // Indicates whether each individual test should be reported during the run
-    // verbose: null,
-
-    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
-    // watchPathIgnorePatterns: [],
-
-    // Whether to use watchman for file crawling
-    // watchman: true,
+module.exports = {
+    collectCoverage: true,
+    collectCoverageFrom: [
+        '**/*.{ts,tsx,vue}',
+        '!**/*.d.ts',
+        '!**/gulpfile.babel.ts',
+        '!**/node_modules/**',
+        '!**/__tests__/**',
+        '!**/__mocks__/**',
+        '!**/vendor/**',
+    ],
+    coverageDirectory: 'coverage',
+    coveragePathIgnorePatterns: [],
+    coverageReporters: ['cobertura', 'text-summary', 'html'],
+    projects: [
+        {
+            ...common,
+            displayName: 'Nuxt',
+            testEnvironment: 'jsdom',
+            testMatch: ['**/__tests__/nuxt/**/*.test.ts'],
+        },
+        {
+            ...common,
+            displayName: 'Electron Renderer',
+            runner: '@jest-runner/electron',
+            testEnvironment: '@jest-runner/electron/environment',
+            testMatch: ['**/__tests__/renderer/**/*.test.ts'],
+        },
+    ],
+    ...common,
 };
