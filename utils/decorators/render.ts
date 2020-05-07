e9f190e1aaaf52cabc5f5cbde7d069e6793b64c1
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-05-07 12:24:29 +0700

    Unit Test: `utils/decorators/render`

--- utils/decorators/render.ts
+++ utils/decorators/render.ts
@@ -1,10 +1,8 @@
-import { camelCase } from 'lodash';
-
 export const Render = <V extends Vue>(getValue: (self: V) => any) => <V>(
     target: V,
     key: keyof V,
 ) => {
-    Object.defineProperty(target, camelCase(`render ${key}`), {
+    Object.defineProperty(target, `${key}_`, {
         get() {
             return getValue(this);
         },
