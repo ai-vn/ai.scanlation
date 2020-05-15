bb02b408b1babc2886b1183699b140682cc7268d
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-05-16 00:49:23 +0700

    Util: `decorators/store.action`

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,5 @@
 export * from './decorators/action';
 export * from './decorators/render';
 export * from './decorators/store.state';
+export * from './decorators/store.action';
 export * from './decorators/watch';
