caa7e0ca3f63ce9e705661cb932203ce0111158b
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-07 14:16:51 +0700

    Unit Test: `actions/folder`

--- __tests__/actions/folder.web.ts
+++ __tests__/actions/folder.web.ts
@@ -1,7 +1,17 @@
+/* eslint-disable no-new */
 import { remote } from 'electron';
+import Vuex from 'vuex';
+import Vue from 'vue';
 import { actions } from '~/actions/actions.import';
+import { explorer, plugins } from '~/store';
+import Explorer from '~/store/explorer';
 
 describe('actions/folder/**', () => {
+    beforeAll(() => {
+        Vue.use(Vuex);
+        new Vuex.Store({ plugins, modules: { explorer: Explorer } });
+    });
+
     it.each([
         [true, []],
         [false, ['folderPath']],
@@ -14,4 +24,14 @@ describe('actions/folder/**', () => {
 
         expect(actions.selectFolder.call).not.toThrow();
     });
+
+    it.each([[''], ['C:/'], ['C:/manga']])(
+        'should go to parent folder',
+        async folderPath => {
+            expect.hasAssertions();
+            explorer.setFolderPath(folderPath);
+            actions.goToParentFolder.call();
+            expect(true).toBeTrue();
+        },
+    );
 });
