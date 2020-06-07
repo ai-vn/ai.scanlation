13a91f8b6b1da8819d0dcf1e298b750ed3f118d6
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-07 14:17:13 +0700

    Unit Test: `components/utilities/input`

--- __tests__/components/utilities/input.nuxt.ts
+++ __tests__/components/utilities/input.nuxt.ts
@@ -1,12 +1,34 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import { shallowMount } from '@vue/test-utils';
+import { Vue } from 'nuxt-property-decorator';
 import input from '~/components/utilities/input.vue';
 
 describe('components/utilities/input', () => {
-    it('should mounted', () => {
-        expect.hasAssertions();
-        const wrapper = shallowMount(input, {
-            propsData: { type: 'number' },
-        });
-        expect(wrapper.vm.$options.name).toStrictEqual('input-');
-    });
+    it.each([[true], [false]])(
+        'should mounted with inGroup=%p',
+        async inGroup => {
+            expect.hasAssertions();
+
+            const wrapper = shallowMount<
+                Vue & {
+                    inGroup: boolean;
+                    blur(): void;
+                    focus(): void;
+                }
+            >(input, {
+                propsData: {
+                    type: 'number',
+                    placeholder: 'placeholder',
+                    min: 0,
+                    max: 10,
+                },
+            });
+
+            wrapper.vm.inGroup = inGroup;
+            wrapper.vm.blur();
+            wrapper.vm.focus();
+            await Vue.nextTick();
+            expect(wrapper.vm.$options.name).toStrictEqual('input-');
+        },
+    );
 });
