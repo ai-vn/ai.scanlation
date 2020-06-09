4ccfb2c0c07a272d9221b5a1e0f7a67b55ca5cf3
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-09 07:07:20 +0700

    Util: `decorators/store.action`

--- types/type.d.ts
+++ types/type.d.ts
@@ -3,6 +3,11 @@ export type NonFunctionPropertyNames<T> = {
 }[keyof T] &
     string;
 
+export type FunctionPropertyNames<T> = {
+    [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never;
+}[keyof T] &
+    string;
+
 export type Payload<T> = {
     value: T;
     oldValue: T;
