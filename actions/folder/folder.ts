6024389efb31047dfc77b947a492389b8e070636
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-18 10:03:24 +0700

    Action: `goToParentFolder`

--- actions/folder/folder.ts
+++ actions/folder/folder.ts
@@ -1,15 +1,32 @@
 import { remote } from 'electron';
+import { dirname } from 'path';
+import { isError } from 'lodash';
 import { ActionItem } from '~/actions/actions.type';
 import { explorer } from '~/store';
+import { attemptAsync } from '~/utils';
 
 export const selectFolder: ActionItem = {
     async call() {
-        const path = await remote.dialog.showOpenDialog({
+        const path = await attemptAsync(remote.dialog.showOpenDialog)({
             properties: ['openDirectory'],
         });
-        const filePath = path.filePaths[0];
-        if (filePath !== undefined) explorer.setFolderPath(filePath);
+        if (isError(path)) return;
+
+        const folderPath = path.filePaths[0];
+        if (folderPath !== undefined) explorer.setFolderPath(folderPath);
     },
     title: 'Open Folder',
     accelerator: 'ctrl+o',
+};
+
+export const goToParentFolder: ActionItem = {
+    call() {
+        if (explorer.folderPath === '') return;
+
+        const parentFolderPath = dirname(explorer.folderPath);
+
+        const isRoot = /^\w:(\\|\/)$/i.test(explorer.folderPath);
+        explorer.setFolderPath(isRoot ? '' : parentFolderPath);
+    },
+    accelerator: 'backspace',
 };
