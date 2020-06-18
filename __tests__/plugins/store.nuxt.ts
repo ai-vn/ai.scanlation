5bfe827b1f4a7f85cfa50feab96bbdc369bfa8f0
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-18 16:07:34 +0700

    Unit Test: `plugins/store`

--- __tests__/plugins/store.nuxt.ts
+++ __tests__/plugins/store.nuxt.ts
@@ -1,13 +1,19 @@
 import pluginStore from '~/plugins/store';
-import { installPlugin } from '~/__tests__/__utils__/nuxt';
 
 describe('plugins/store', () => {
     beforeAll(() => {
+        jest.mock('~/actions/actions.import', () => ({
+            actions: {},
+        }));
+
         window.onNuxtReady = (callback: () => void) => callback();
     });
 
-    it('should pass', () => {
+    it('should pass', async () => {
         expect.hasAssertions();
+
+        const { installPlugin } = await import('~/__tests__/__utils__/nuxt');
+
         expect(() => {
             installPlugin(pluginStore);
         }).not.toThrow();
