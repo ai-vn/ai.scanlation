fe09f85f242f2613c308d28229b51001ca0cea28
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-23 11:39:12 +0700

    Module: Explorer

---
+++ modules/explorer/analyze/files/files.ts
@@ -0,0 +1,37 @@
+import { stat } from 'fs';
+import { extname, join } from 'path';
+import { isError } from 'lodash';
+import { getFolderType } from '../utils/folder.extension';
+import { attemptPromisify } from '~/utils';
+import { FileSystemObject } from '~/modules/explorer/types';
+
+export const analyze = async (
+    folderPath: string,
+    name: string,
+): Promise<FileSystemObject | null> => {
+    const path = join(folderPath, name);
+
+    const data = await attemptPromisify(stat)(path);
+    if (isError(data)) return null;
+
+    const result: FileSystemObject = {
+        index: 0,
+        key: path,
+        name,
+        stat: data,
+        path,
+        ext: '',
+        time: data.mtime,
+        isFolder: true,
+    };
+    if (data.isDirectory()) {
+        result.isFolder = true;
+        result.ext = getFolderType(name);
+    }
+    if (data.isFile()) {
+        result.isFolder = false;
+        result.size = data.size;
+        result.ext = extname(`_${name}`).replace(/^\./, '').toLocaleLowerCase();
+    }
+    return result;
+};
---
+++ modules/explorer/analyze/images/src/psd.ts
@@ -0,0 +1,25 @@
+// http://www.adobe.com/devnet-apps/photoshop/fileformatashtml/#50577409_pgfId-1037450
+
+import { AnalyzeImage } from '../type';
+
+const colorMap: Record<string, string | number | undefined> = {
+    0: 24,
+    1: 8,
+    2: 8,
+    3: 24,
+    4: 24,
+    7: 'Multi',
+    8: 'Duotone',
+    9: 'Lab',
+};
+
+export const psd: AnalyzeImage = {
+    match: buffer => buffer.toString('hex', 0, 4) === '38425053',
+    data: buffer => ({
+        dimensions: {
+            x: buffer.readUIntBE(18, 4),
+            y: buffer.readUIntBE(14, 4),
+        },
+        color: `PSD-${colorMap[buffer.readUIntBE(24, 2)]}`,
+    }),
+};
