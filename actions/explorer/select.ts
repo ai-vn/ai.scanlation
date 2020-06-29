1e3f7d6c616dd4d52647516c677d2e31139a66d6
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-29 23:43:59 +0700

    Actions: `explorer/select`

---
+++ actions/explorer/select.ts
@@ -0,0 +1,19 @@
+import { ActionItem } from '~/actions/actions.type';
+import { isExplorer } from '~/actions/conditions';
+import { explorer } from '~/store';
+
+export const selectAll: ActionItem = {
+    call() {
+        const { files, folders } = explorer;
+        if (files.length === 0) return;
+
+        const isSelectedAll = !explorer.files.some(f => f.selected === false);
+        explorer.setData({
+            files: files.map(file => ({ ...file, selected: !isSelectedAll })),
+            folders,
+        });
+    },
+    condition: isExplorer,
+    title: 'Select All',
+    accelerator: 'ctrl+a',
+};
