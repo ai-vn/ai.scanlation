bfd544d544a3fded5db0f9eda8ddfe9dc3212e0c
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-06-29 15:24:39 +0700

    Actions: `explorer/item`

--- actions/actions.import.ts
+++ actions/actions.import.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-cycle */
 import * as dev from './electron/dev';
 import * as folder from './folder/folder';
+import * as item from './explorer/item';
 import * as routers from './pages/routers';
 import * as shortcuts from './shortcut/reset';
 import * as windows from './electron/windows';
@@ -8,6 +9,7 @@ import * as windows from './electron/windows';
 export const actions = {
     ...dev,
     ...folder,
+    ...item,
     ...routers,
     ...shortcuts,
     ...windows,
