32a14d0c229508cb6f33b15c49274de31e6361bd
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-08-03 16:25:06 +0700

    Tooltip: Update sidebar

--- components/sidebar/sidebar.vue
+++ components/sidebar/sidebar.vue
@@ -1,19 +1,34 @@
 <template>
     <div class="sidebar">
-        <n-link v-tooltip.right="'Home'" to="/" exact>
+        <n-link v-tooltip="tooltip('Home')" to="/" exact>
             <icon- i="home" />
         </n-link>
-        <n-link v-tooltip.right="'Explorer'" to="/explorer" exact>
+        <n-link v-tooltip="tooltip('Explorer')" to="/explorer" exact>
             <icon- i="file" />
         </n-link>
-        <n-link v-tooltip.right="'Reader'" to="/reader">
+        <n-link v-tooltip="tooltip('Reader')" to="/reader" exact>
             <icon- i="book-open" />
         </n-link>
-        <n-link v-tooltip.right="'Setting'" to="/settings" class="mt-auto">
+        <n-link v-tooltip="tooltip('Setting')" to="/settings" class="mt-auto">
             <icon- i="settings" />
         </n-link>
     </div>
 </template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+import { TooltipSettings } from 'v-tooltip';
+
+@Component({ name: 'sidebar-' })
+export default class extends Vue {
+    tooltip = (content: string): TooltipSettings => {
+        return {
+            content,
+            placement: 'right',
+            trigger: 'hover',
+        };
+    };
+}
+</script>
 <style lang="postcss">
 .sidebar {
     $sidebar-size: 3rem;
