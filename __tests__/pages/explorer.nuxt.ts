965b1e77637973ba320436d81e4ebab1c2e7a14e
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-09-07 10:14:32 +0700

    Component: `explorer`

--- __tests__/pages/explorer.nuxt.ts
+++ __tests__/pages/explorer.nuxt.ts
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
-/* eslint-disable no-new */
 import { shallowMount } from '@vue/test-utils';
 import Vue from 'vue';
-import Vuex from 'vuex';
-import { mock } from '~/__tests__/__utils__/mock';
+import { store } from '~/__tests__/__utils__';
 import table from '~/components/table/table.vue';
+import button from '~/components/utilities/button.vue';
 import group from '~/components/utilities/group.vue';
 import icon from '~/components/utilities/icon.vue';
 import input from '~/components/utilities/input.vue';
@@ -12,30 +10,13 @@ import '~/plugins/v-tooltip';
 
 describe('pages/explorer', () => {
     beforeAll(async () => {
-        jest.mock('~/actions/actions.import', () => ({
-            actions: {
-                explorerSelectFolder: { call: () => {} },
-                explorerGoToParentFolder: { call: () => {} },
-                explorerReload: { call: () => {} },
-            },
-        }));
-
-        mock('~/modules/explorer', ['table']);
-
-        const { default: Explorer } = await import('~/store/explorer');
-        const { plugins } = await import('~/store');
-        const { default: button } = await import(
-            '~/components/utilities/button.vue'
-        );
-
-        Vue.use(Vuex);
-        new Vuex.Store({ plugins, modules: { explorer: Explorer } });
+        await store('explorer');
 
         Vue.component('button-', button);
+        Vue.component('group-', group);
         Vue.component('icon-', icon);
         Vue.component('input-', input);
         Vue.component('table-', table);
-        Vue.component('group-', group);
     });
 
     it('should', async () => {
