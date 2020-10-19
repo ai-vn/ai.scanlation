ed799d2ee78356407212a51c40fd31d45372b8f2
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-10-19 15:06:54 +0700

    Plugins: `components-auto`: Import from `/pages`

--- plugins/components-auto.ts
+++ plugins/components-auto.ts
@@ -1,10 +1,15 @@
 import Vue from 'vue';
 
-const requireComponent = require.context('../components', true, /.vue$/);
+const importComponents = (
+    requireContext: ReturnType<typeof require.context>,
+) => {
+    requireContext.keys().forEach((fileName): void => {
+        const componentConfig = requireContext(fileName);
+        const pattern = /^\.[/\w-]*\/([\w-]+)\.vue$/i;
+        const name = fileName.replace(pattern, '$1');
+        Vue.component(`${name}-`, componentConfig.default || componentConfig);
+    });
+};
 
-requireComponent.keys().forEach((fileName): void => {
-    const componentConfig = requireComponent(fileName);
-    const pattern = /^\.[/\w-]*\/([\w-]+)\.vue$/i;
-    const name = fileName.replace(pattern, '$1');
-    Vue.component(`${name}-`, componentConfig.default || componentConfig);
-});
+importComponents(require.context('../components', true, /.vue$/));
+importComponents(require.context('../pages', true, /-src\/.*.vue$/));
