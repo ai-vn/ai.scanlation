e082f591f52ac62bd7b77bee69ba40a7c200d2ce
Phạm Hồng Phúc <phamhongphuc.info@gmail.com>
2020-11-07 09:26:33 +0700

    Tailwind: Create Utility `group-space`

---
+++ components/utilities/group-space.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="group-space">
+        <div :class="[`group-space-${space}`, childClass]">
+            <slot />
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+
+export default defineComponent({
+    name: 'button-',
+    props: {
+        space: { required: true, type: Number },
+        childClass: { default: '', type: String },
+    },
+});
+</script>
+<style lang="postcss">
+.group-space {
+    > div {
+        @apply flex;
+    }
+}
+</style>
